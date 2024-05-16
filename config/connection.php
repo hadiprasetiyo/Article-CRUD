e4659fd5c5b57f84c72b7133ac1aaef63ede9670
hadiprasetiyo <hadiiyok01@gmail.com>
2024-05-16 18:44:41 +0800

    feat: add connection.php for database connection

---
+++ config/connection.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$database = "article";
+$username = "root";
+$password = "";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+?>
